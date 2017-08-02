f53a025b3529beeca2f48909232adaaa5dc48b70
David Kahle <david.kahle@gmail.com>
2017-08-02 13:06:23 -0400

    genesis

---
+++ 2017 SIAM Talk.R
@@ -0,0 +1,15 @@
+setwd("/Users/david_kahle/Dropbox/Baylor University/Employment/Conferences/SIAM/2017/2017 SIAM Talk")
+
+
+##### installing stuff
+########################################
+
+if(!requireNamespace("devtools")) install.packages("devtools")
+devtools::install_github("dkahle/mpoly", ref = "670645f")
+devtools::install_github("dkahle/latter", ref = "9d2fd27")
+devtools::install_github("dkahle/tropical", ref = "5599df2")
+devtools::install_github("coneill-math/m2r", ref = "572aadd")
+devtools::install_github("dkahle/algstat", ref = "1f063ab")
+
+
+
